c8de1207ca61569c9a7492f2bc2a0a5f00ed2d43
Caroline Edgren <caroline.edgren88@gmail.com>
2024-04-08 00:27:05 +0200

    commit

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -19,40 +19,67 @@ const TaskCard = ({ index, columnPosition }) => {
   const moveTaskToRight = () => {
     if (columnPosition < 2) {
       setTasks((prevTasks) => {
-        const newTasks = [...prevTasks];
-        newTasks[columnPosition + 1].tasks.push(tasks[columnPosition].tasks[index]);
-        handleDelete();
-        return newTasks;
+        const taskToMove = tasks[columnPosition].tasks[index];
+        const updatedTasks = prevTasks.map((column, columnIndex) => {
+          if (columnIndex === columnPosition) {
+            return {
+              ...column,
+              tasks: column.tasks.filter((_, taskIndex) => taskIndex !== index)
+            };
+          } else if (columnIndex === columnPosition + 1) {
+            return {
+              ...column,
+              tasks: [...column.tasks, taskToMove]
+            };
+          } else {
+            return column;
+          }
+        });
+        return updatedTasks;
       });
     }
   };
-
+  
   const moveTaskToLeft = () => {
     if (columnPosition > 0) {
       setTasks((prevTasks) => {
-        const newTasks = [...prevTasks];
-        newTasks[columnPosition - 1].tasks.push(tasks[columnPosition].tasks[index]);
-        handleDelete();
-        return newTasks;
+        const taskToMove = tasks[columnPosition].tasks[index];
+        const updatedTasks = prevTasks.map((column, columnIndex) => {
+          if (columnIndex === columnPosition) {
+            return {
+              ...column,
+              tasks: column.tasks.filter((_, taskIndex) => taskIndex !== index)
+            };
+          } else if (columnIndex === columnPosition - 1) {
+            return {
+              ...column,
+              tasks: [...column.tasks, taskToMove]
+            };
+          } else {
+            return column;
+          }
+        });
+        return updatedTasks;
       });
     }
   };
 
   return (
     <div>
-      <div className="task-row" key={index}>
-        <h3>{tasks[columnPosition].tasks[index].taskTitle}</h3>
-        <h3>{tasks[columnPosition].tasks[index].content}</h3>
-        <div className="icons">
-          <RiCloseCircleLine
-            className="delete-icon"
-            onClick={handleDelete}
-          />
-          {columnPosition <= 1 && <RiArrowRightLine onClick={moveTaskToRight} />}
-          {columnPosition === 2 && <RiArrowLeftLine onClick={moveTaskToLeft} />}
-        </div>
+    <div className="task-row" key={index}>
+      <h3>{tasks[columnPosition].tasks[index].taskTitle}</h3>
+      <h3>{tasks[columnPosition].tasks[index].content}</h3>
+      <div className="icons">
+        <RiCloseCircleLine
+          className="delete-icon"
+          onClick={handleDelete}
+        />
+        {columnPosition === 1 && <RiArrowLeftLine onClick={moveTaskToLeft} />}
+        {columnPosition === 2 && <RiArrowLeftLine onClick={moveTaskToLeft} />}
+        {columnPosition <= 1 && <RiArrowRightLine onClick={moveTaskToRight} />}
       </div>
     </div>
+  </div>
   );
 };
 
